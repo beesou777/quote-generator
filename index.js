eb712c9efbc542a039a0ec157d9a7c2721b895f6
beesou777 <sbeesou@gmail.com>
2024-09-02 17:28:26 +0545

    soiefh

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-require("dotenv").config();
 const axios = require('axios');
 const ULR = "https://api.quotable.io/random"
 const { Client, GatewayIntentBits } = require('discord.js');
-const express = require('express');
 
-const app = express();
-
-const PORT = process.env.PORT || 3000
-
+const clients = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 let quote;
-
-
-app.get('/', (req, res) => {
-    res.send(quote);
-});
-
 
 async function generateQuote() {
     try {
@@ -28,43 +17,17 @@ async function generateQuote() {
     }
 }
 
-
-const clients = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-
-clients.on("ready", () => {
-    clients.application.commands.create({
-        name: "quote",
-        description: "Get a random quote",
-    })
-});
-
 clients.login(process.env.APIKEY).catch((err) => {
     console.error('Failed to log in:', err);
 });
 
-
-setInterval(generateQuote, 1000);
-
-// clients.on('messageCreate', (msg) => {
-//     try {
-//         console.log(msg)
-//         let splitMessage = msg.content.split(" ")[0]
-//         if (splitMessage.trim() === 'quote') {
-//             msg.reply(quote);
-//         }
-//     } catch (error) {
-//         console.log(error)
-//     }
-// });
-clients.on("interactionCreate",async(msg)=>{
-    try {
-        if(!msg.isCommand()) return
-        if(msg && msg.commandName === "quotes") return await msg.reply(quote);
-    } catch (error) {
-        console.log(error)
+clients.on("ready", async(bot) => {
+    console.log(`Logged in as ${bot.user.tag}!`);
+    await generateQuote();
+    const channel = clients.channels.cache.get("1279091593743437908");
+    if(channel && quote){
+        channel.send(quote);
+    }else{
+        console.log("channel not found")
     }
-})
-
-app.listen(PORT,()=>{
-    console.log('listening on port 3000')
-});
+});  
